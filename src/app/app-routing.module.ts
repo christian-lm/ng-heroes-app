104e86fed2bac7bfe4f44c4f8b807971255dec89
Christian Lozano <christian.lozano@neoris.com>
2022-10-21 18:26:07 +0200

    - Se definen las rutas hijas y se declara el LazyLoad de heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ErrorPageComponent} from "./shared/error-page/error-page.component";
 import {AuthModule} from "./auth/auth.module";
+import {HeroesModule} from "./heroes/heroes.module";
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(() => AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(() => HeroesModule)
   },
   {
     path: '404',
