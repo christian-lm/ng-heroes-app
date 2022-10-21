56adab30d74f1c10648289ad2973b4c3b5ba7e8b
Christian Lozano <christian.lozano@neoris.com>
2022-10-21 17:48:38 +0200

    - Creación de modulos y componentes

---
+++ src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './registro.component.html'
+})
+export class RegistroComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './agregar.component.html'
+})
+export class AgregarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
