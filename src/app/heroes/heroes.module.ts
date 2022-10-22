7d7acb0bb2a022ae026f9717578e3c691231f2cb
Christian Lozano <christian.lozano@neoris.com>
2022-10-22 11:32:29 +0200

    - Se instala el Angular-Flex y se importa en el modulo de heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,7 @@ import {HeroeComponent} from './pages/heroe/heroe.component';
 import {HomeComponent} from './pages/home/home.component';
 import {ListadoComponent} from './pages/listado/listado.component';
 import {HeroesRoutingModule} from "./heroes-routing.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import {HeroesRoutingModule} from "./heroes-routing.module";
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     HeroesRoutingModule
   ]
 })
