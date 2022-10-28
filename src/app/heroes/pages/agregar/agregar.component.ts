7b0511a4473a9078a627f3026572053fc59d5b7b
Christian Lozano <christian.lozano@neoris.com>
2022-10-28 19:32:13 +0200

    - Se hace el diseño de la pantalla de agregar heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,10 +1,31 @@
 import {Component, OnInit} from '@angular/core';
+import {Heroe, Publisher} from "../../interfaces/heroe.interface";
 
 @Component({
   selector: 'app-add',
   templateUrl: './agregar.component.html'
 })
 export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC',
+      desc: Publisher.DCComics
+    },
+    {
+      id: 'Marvel',
+      desc: Publisher.MarvelComics
+    }
+  ];
+
+  heroe: Heroe =
+    {
+      alter_ego: "",
+      characters: "",
+      first_appearance: "",
+      publisher: Publisher.MarvelComics,
+      superhero: ""
+    }
 
   constructor() {
   }
