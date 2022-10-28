6713d26db5871524c2e7c0c31c1216ebfeb2a325
Christian Lozano <christian.lozano@neoris.com>
2022-10-28 17:38:57 +0200

    - Se activan los botones 'Leer Mas' y 'Editar'

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +7,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    console.log(this.route.snapshot.paramMap.get('id'))
   }
 
 }
